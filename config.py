d93acef1134600c2f85d78d9502e53f86ec38fed
Joshua Scina <jscina@Joshuas-MacBook-Pro.local>
2025-03-24 12:21:07 -0400

    add in camera fallback for non raspi devices

--- config.py
+++ config.py
@@ -3,3 +3,5 @@ FRAME_HEIGHT = 240
 
 EYE_CHANNELS = {"left_x": 0, "left_y": 1, "right_x": 2, "right_y": 3}
 EXTRA_CHANNELS = {"extra_1": 8, "extra_2": 9}
+
+USE_USB_CAMERA = False
--- main.py
+++ main.py
@@ -12,33 +12,26 @@ def main() -> None:
     center_servos()
     last_face_time = time.time()
 
-    camera, stream = camera_stream()
-    with camera:
-        with stream:
-            for frame_data in camera.capture_continuous(
-                stream, format="bgr", use_video_port=True
-            ):
-                frame = frame_data.array
-                faces = detect_faces(frame)
+    for frame in camera_stream():
+        faces = detect_faces(frame)
 
-                # Callback function to update the last face detection time
-                def on_face_detected() -> None:
-                    nonlocal last_face_time
-                    last_face_time = time.time()
+        # Callback function to update last face detection time
+        def on_face_detected() -> None:
+            nonlocal last_face_time
+            last_face_time = time.time()
 
-                face_detected = draw_faces(frame, faces, on_face_detected)
-                draw_quadrants(frame)
+        face_detected = draw_faces(frame, faces, on_face_detected)
+        draw_quadrants(frame)
 
-                cv2.imshow("Live Footage", frame)
-                stream.truncate(0)
-                stream.seek(0)
+        cv2.imshow("Live Footage", frame)
 
-                if cv2.waitKey(1) & 0xFF == ord("q"):
-                    break
+        if cv2.waitKey(1) & 0xFF == ord("q"):
+            break
 
-                if not face_detected and (time.time() - last_face_time > 5):
-                    center_servos()
-                    last_face_time = time.time()
+        # If no face for 5s, recenter servos
+        if not face_detected and (time.time() - last_face_time > 5):
+            center_servos()
+            last_face_time = time.time()
 
     cv2.destroyAllWindows()
 
