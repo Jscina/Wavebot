13ac2aadabd50735ab4a2c44e1d3d341b8d3dc04
jscina <joshscina@gmail.com>
2025-03-26 13:44:51 -0400

    add servo calibration script

--- wavebot/__init__.py
+++ wavebot/__init__.py
@@ -1,5 +1,5 @@
 from .camera import camera_stream
-from .config import logger
+from .config import logger, Channel
 from .vision import detect_faces, draw_faces, draw_quadrants
 from .servos import center_servos, set_servo_angle, update_servos
 
@@ -12,4 +12,5 @@ __all__ = [
     "set_servo_angle",
     "update_servos",
     "logger",
+    "Channel",
 ]
